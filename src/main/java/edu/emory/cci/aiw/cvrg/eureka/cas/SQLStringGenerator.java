3ccb6b13d26dba3088f5a5ae6473a3b2886e7404
Miao Ai <miaoai2010@gmail.com>
2016-01-25 17:14:06 -0500

    Logger changes for the fix of hardcoded postgreSQL password

--- src/main/java/edu/emory/cci/aiw/cvrg/eureka/cas/SQLStringGenerator.java
+++ src/main/java/edu/emory/cci/aiw/cvrg/eureka/cas/SQLStringGenerator.java
@@ -4,21 +4,30 @@
  * and open the template in the editor.
  */
 package edu.emory.cci.aiw.cvrg.eureka.cas;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  *
  * @author Miao Ai
  */
 public class SQLStringGenerator {
-    private String jndiName;
+	/**
+	 * The class level logger.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(
+			SQLStringGenerator.class);
+        
+        private String jndiName;
 
 
 	public String getJndiName() {
@@ -45,24 +54,30 @@ public class SQLStringGenerator {
                                 result="select a2.password from users a1 join local_users a2 on (a1.id=a2.id) where a1.username=? and a1.active='true'";
                         }
                 } catch (AuthenticationException ex) {
-                        Logger.getLogger("Error in Authentication Exception: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                        LOGGER.error("Error authenticating user: "
+                                        +"user credentials is invalid or naming/directory service failed.",
+                                        ex.getMessage());
                 } catch ( NamingException ex) {
-                        Logger.getLogger("Error in Naming Exception: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                        LOGGER.error("NamingException in initializing context.",
+                                        ex.getMessage());
                 } catch (SQLException ex) {
-                        Logger.getLogger("Error in SQL Exeception: "+SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                        LOGGER.error("SQLException in getting data source connetion or driver name.",
+                                        ex.getMessage());
                 } finally {
                         if (initContext != null) {
                                 try {
                                     initContext.close();
                                 } catch (NamingException ex) {
-                                    Logger.getLogger("Error in Naming Exception for closing initContext: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                                    LOGGER.error("NamingException in closing context.",
+                                                    ex.getMessage());
                                 }
                         }  
                         if (connection != null) {
                                 try {
                                     connection.close();
                                 } catch (SQLException ex) {
-                                    Logger.getLogger("Error in SQL Exeception for closing connection: "+SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                                    LOGGER.error("SQLException in closing connection.",
+                                                    ex.getMessage());
                                 }
                         }
                 }
