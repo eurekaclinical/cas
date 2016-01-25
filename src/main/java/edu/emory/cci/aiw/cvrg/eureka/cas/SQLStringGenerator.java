3ef321494bb997fa4822d9ddcd278bd9a7c50395
Miao Ai <miaoai2010@gmail.com>
2016-01-25 13:30:42 -0500

    Modifications for the fix of hardcoded password with postgreSQL issue

--- src/main/java/edu/emory/cci/aiw/cvrg/eureka/cas/SQLStringGenerator.java
+++ src/main/java/edu/emory/cci/aiw/cvrg/eureka/cas/SQLStringGenerator.java
@@ -29,38 +29,40 @@ public class SQLStringGenerator {
                 this.jndiName  = jndiName;        
         }
 
-        public String getUserVerficationSQLString()
-        {
+        public String getUserVerficationSQLString(){
                 String result="select a2.password from users a1 join local_users a2 on (a1.id=a2.id) where a1.username=? and a1.active=1";
                 String driverName;
                 Connection connection = null;
-                try
-                {
-                        Context initContext = new InitialContext();
+                Context initContext = null;
+                
+                try {
+                        initContext = new InitialContext();
                         DataSource ds = (DataSource)initContext.lookup(jndiName);
 
                         connection = ds.getConnection();
                         driverName = connection.getMetaData().getDriverName();
-                        if(driverName.toLowerCase().contains("postgresql"))
-                        {
+                        if(driverName.toLowerCase().contains("postgresql")) {
                                 result="select a2.password from users a1 join local_users a2 on (a1.id=a2.id) where a1.username=? and a1.active='true'";
                         }
-                }
-                catch (AuthenticationException ex) {
-                     Logger.getLogger("Error in Authentication Exception: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);;
-                }
-                catch ( NamingException ex){
+                } catch (AuthenticationException ex) {
+                        Logger.getLogger("Error in Authentication Exception: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                } catch ( NamingException ex) {
                         Logger.getLogger("Error in Naming Exception: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
-                } 
-                catch (SQLException ex) {
+                } catch (SQLException ex) {
                         Logger.getLogger("Error in SQL Exeception: "+SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                finally {
-                        if (connection != null){
+                } finally {
+                        if (initContext != null) {
+                                try {
+                                    initContext.close();
+                                } catch (NamingException ex) {
+                                    Logger.getLogger("Error in Naming Exception for closing initContext: " + SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                        }  
+                        if (connection != null) {
                                 try {
                                     connection.close();
                                 } catch (SQLException ex) {
-                                    Logger.getLogger("Error in SQL Exeception for closing the connection: "+SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                                    Logger.getLogger("Error in SQL Exeception for closing connection: "+SQLStringGenerator.class.getName()).log(Level.SEVERE, null, ex);
                                 }
                         }
                 }
